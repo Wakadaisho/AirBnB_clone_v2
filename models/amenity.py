7ee314fd64ae2971a793218be1fffef1a5e29c64
Abdia Elema <gababoabdiaelema@gmail.com>
2023-08-22 12:10:32 -0700

    doc

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
+"""The Amenity Module for HBNB project """
 from os import getenv
 from models.base_model import BaseModel, Base
 from models.place import place_amenity
